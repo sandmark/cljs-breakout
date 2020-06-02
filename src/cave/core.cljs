e34a804b6423945d9aca5607726af64540a5f40d
sandmark <sandmark.m@gmail.com>
2020-06-03 00:20:19 +0900

    Make the ball bounding

--- src/cave/core.cljs
+++ src/cave/core.cljs
@@ -1,14 +1,18 @@
 (ns cave.core)
 
-(defonce app-state (atom {:canvas nil
-                          :ctx    nil
-                          :x      nil
-                          :y      nil
-                          :dx     nil
-                          :dy     nil}))
+(defonce app-state (atom {:canvas      nil
+                          :ctx         nil
+                          :x           nil
+                          :y           nil
+                          :width       nil
+                          :height      nil
+                          :dx          2
+                          :dy          -2
+                          :ball-radius 10}))
 
-(defn draw-ball [{:keys [ctx x y]}]
 (def PI (.-PI js/Math))
+
+(defn draw-ball [{:keys [ctx x y ball-radius]}]
   (set! (.-fillStyle ctx) "#0095DD")
   (doto ctx
     (.beginPath)
@@ -16,23 +20,32 @@
     (.fill)
     (.closePath)))
 
-(defn draw [state]
-  (let [{:keys [canvas ctx dx dy]} @state
-        width                      (.-width canvas)
-        height                     (.-height canvas)]
+(defn out-of-border? [ball-radius x dx border]
+  (or (> (+ x dx) (- border ball-radius))
+      (< (+ x dx) ball-radius)))
+
+(defn draw []
+  (let [{:keys [ctx x y dx dy width height ball-radius]} @app-state]
     (.clearRect ctx 0 0 width height)
-    (draw-ball @state)
-    (swap! state update :x + dx)
-    (swap! state update :y + dy)))
+    (draw-ball @app-state)
+
+    (cond
+      (out-of-border? ball-radius x dx width)  (swap! app-state update :dx -)
+      (out-of-border? ball-radius y dy height) (swap! app-state update :dy -))
+
+    (swap! app-state update :x + dx)
+    (swap! app-state update :y + dy)))
 
 (defn start []
   (let [canvas (.getElementById js/document "app")
+        width  (.-width canvas)
+        height (.-height canvas)
         system {:canvas canvas
                 :ctx    (.getContext canvas "2d")
-                :x      (/ (.-width canvas) 2)
-                :y      (- (.-height canvas) 30)
-                :dx     2
-                :dy     -2}]
+                :width  width
+                :height height
+                :x      (/ width 2)
+                :y      (- height 30)}]
     (swap! app-state merge system)
     (swap! app-state assoc :timer (js/setInterval draw 10))))
 
