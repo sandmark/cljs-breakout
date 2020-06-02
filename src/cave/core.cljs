776fef063f6009c0aa12eccaed360f7ba8676f21
sandmark <sandmark.m@gmail.com>
2020-06-03 08:50:14 +0900

    Add static paddle

--- src/cave/core.cljs
+++ src/cave/core.cljs
@@ -1,9 +1,13 @@
 (ns cave.core)
 
-(defonce app-state (atom {:dx          2  ;; Direction of Ball-x
-                          :dy          -2 ;; Direction of Ball-y
-                          :ball-radius 10
-                          }))
+(def config
+  {:paddle-height 10
+   :paddle-width  75
+   :dx            2  ;; Direction of Ball-x
+   :dy            -2 ;; Direction of Ball-y
+   :ball-radius   10})
+
+(defonce app-state (atom {}))
 
 (def PI (.-PI js/Math))
 
@@ -12,6 +16,14 @@
   (doto ctx
     (.beginPath)
     (.arc x y ball-radius 0 (* PI 2))
+    (.fill)
+    (.closePath)))
+
+(defn draw-paddle [{:keys [ctx height paddle-height paddle-width paddle-x]}]
+  (set! (.-fillStyle ctx) "#0095DD")
+  (doto ctx
+    (.beginPath)
+    (.rect paddle-x (- height paddle-height) paddle-width paddle-height)
     (.fill)
     (.closePath)))
 
@@ -27,15 +39,19 @@
         (when (apply out-of-border? params)
           (swap! app-state update k -))))))
 
+(defn- move-ball []
+  (doseq [[pos dir] [[:x :dx] [:y :dy]]]
+    (swap! app-state update pos + (dir @app-state))))
+
 (defn draw []
-  (let [{:keys [ctx dx dy width height]} @app-state]
+  (let [{:keys [ctx width height]} @app-state]
     (.clearRect ctx 0 0 width height)
     (draw-ball @app-state)
+    (draw-paddle @app-state)
 
     (bound [:dx :dy])
 
-    (swap! app-state update :x + dx)
-    (swap! app-state update :y + dy)))
+    (move-ball)))
 
 (defn- canvas []
   (let [canvas (.getElementById js/document "app")]
@@ -46,9 +62,10 @@
 
 (defn start []
   (let [{:keys [width height] :as params} (canvas)
-        pos                               {:x (/ width 2)
-                                           :y (- height 30)}]
-    (reset! app-state (merge params pos))
+        positions                         {:x        (/ width 2)
+                                           :y        (- height 30)
+                                           :paddle-x (/ (- width (:paddle-width config)) 2)}]
+    (reset! app-state (merge config params positions))
     (swap! app-state assoc :timer (js/setInterval draw 10))))
 
 (defn ^:export init []
